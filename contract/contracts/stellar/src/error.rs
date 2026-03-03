80a5c500817fd5a5e9c62da87c1ed03d9135121b
kelly-musk <ogunjioforkelechi@gmail.com>
2026-03-03 10:22:40 +0100

    Deleted unused files

--- contract/contracts/stellar/src/error.rs
+++
@@ -1,9 +0,0 @@
-use soroban_sdk::contracterror;
-
-#[contracterror]
-#[derive(Copy, Clone, Debug, Eq, PartialEq)]
-pub enum ContractError {
-    Unauthorized = 1,
-    InsufficientBalance = 2,
-    InvalidAmount = 3,
-}
